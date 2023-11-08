e1ef75a25409396e08b6be230c063ca3c3120997
golangkhoa <145892397+golangkhoa@users.noreply.github.com>
2023-11-08 21:14:02 +0700

    rebuild exe

--- cmd/genproject.go
+++ cmd/genproject.go
@@ -42,10 +42,20 @@ func generateProject(cmd *cobra.Command, args []string) {
 
 	file.WriteString(`package main
 
-import "fmt"
-	
+import (
+    "net/http"
+
+    "github.com/go-chi/chi/v5"
+    "github.com/go-chi/chi/v5/middleware"
+)
+
 func main() {
-	fmt.Println("Hello world!")
+    r := chi.NewRouter()
+    r.Use(middleware.Logger)
+    r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+        w.Write([]byte("Hello World!"))
+    })
+    http.ListenAndServe(":3000", r)
 }
 `)
 
